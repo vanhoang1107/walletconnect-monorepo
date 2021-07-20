b38dcff472b58c26d5f40f083013700394df20b0
Dinh Van Hoang <hoangdv@snapinnovations.vn>
2021-07-21 00:14:45 +0700

    [relay] Support graceful shutdown.

--- servers/relay/src/index.ts
+++ servers/relay/src/index.ts
@@ -1,11 +1,53 @@
+import * as Sentry from "@sentry/node";
+import { LogLevel as SentryLogLevel } from "@sentry/types";
 import config from "./config";
 import { HttpService } from "./http";
+
+
+function initSentry() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    debug: config.debug,
+    logLevel: SentryLogLevel.Error,
+    maxBreadcrumbs: 30,
+  })
+}
+initSentry()
 
 const { app } = new HttpService({
   logger: config.debug ? "debug" : "warn",
 });
 
+// Due to Pino Sentry has init Sentry the 2nd time.
+// We need to init the 3rd time to override the empty config.
+app.addHook('onReady', done => {
+  initSentry()
+  done()
+})
+
+async function onCloseSignal(signal) {
+  console.info(`Signal ${signal} received.`)
+  console.log('App is being closed...')
+
+  const closeErr = await app.close()
+  if (closeErr) {
+    console.log('Close app failed with error:', closeErr)
+  }
+
+  console.log("Sentry flush in 2s...")
+  const flushOK = await Sentry.flush(2000)
+  if (!flushOK) {
+    console.log("Sentry flush failed")
+  }
+  console.log('App has been closed!')
+
+  process.exit(closeErr ? 1 : 0)
+}
+
+process.on('SIGTERM', onCloseSignal)
+process.on('SIGINT', onCloseSignal)
+
 app.listen(+config.port, config.host, (err, address) => {
   if (!config.debug) app.log.info(`Server listening on ${address}`);
   if (err) throw err;
-});
+})
